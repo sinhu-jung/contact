f1c7751a0f30ecc2b84039666798a757ee041075
SinHuJung <tlsgn26@naver.com>
2021-11-30 15:56:16 +0900

    change axios baseURL

--- src/Service.js
+++ src/Service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:4000/contacts'
+const baseURL = 'https://sinhu-contacts-server.herokuapp.com/contacts'
 
 class service {
   getAll(){
