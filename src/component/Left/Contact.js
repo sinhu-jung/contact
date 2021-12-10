883ae0c00d19fee49baaee465929b751026d3fd5
SinHuJung <tlsgn26@naver.com>
2021-12-10 16:01:00 +0900

    onfocus

--- src/component/Left/Contact.js
+++ src/component/Left/Contact.js
@@ -5,7 +5,7 @@ const Contact = ({id, name, phoneNumber, setSelectId, selectId}) => {
     return(
         <li className={id === selectId ? "itemSelected" : "item"}>
             <button className={id === selectId ? "contactSelect" : "contactNotSelect"} 
-                               onClick={() => setSelectId(id)}>
+                               onFocus={() => setSelectId(id)}>
                 <div style={{padding:'5px'}}>{name}</div>
                 <div style={{padding:'5px'}}>{phoneNumber}</div>
             </button>
